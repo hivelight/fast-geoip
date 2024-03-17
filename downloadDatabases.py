9ceb80e77ed83402e4a855ba0bba9695e94a5181
Xabi Errotabehere <6732440+keinu@users.noreply.github.com>
2024-03-17 18:55:09 +1300

    Update downloadDatabases.py

--- downloadDatabases.py
+++ downloadDatabases.py
@@ -1,5 +1,6 @@
 import urllib.request, os, zipfile, glob, shutil
 import processGeoIpCsv as geoip
+from base64 import b64encode
 
 ZIP_FILENAME = "geolite.zip"
 TEMPORAL_EXTRACTED_DIR = "geoip"
@@ -9,8 +10,25 @@ def rmtree(directory):
 
 # urllib.request.urlretrieve ("https://geoip.maxmind.com/app/geoip_download?edition_id=GeoLite2-City-CSV&suffix=zip&license_key=" + os.environ["MAXMIND_LICENSE_KEY"], ZIP_FILENAME)
 
-urllib.request.urlretrieve ("https://download.maxmind.com/geoip/databases/GeoLite2-City-CSV/download?suffix=zip&license_key=" + os.environ["MAXMIND_LICENSE_KEY"], ZIP_FILENAME)
 
+# Basic Authentication credentials
+username =  os.environ["ACCOUNT_ID"]
+password =  os.environ["MAXMIND_LICENSE_KEY"]
+
+# Encode username and password in base64
+credentials = f"{username}:{password}".encode('ascii')
+base64_credentials = b64encode(credentials).decode('ascii')
+
+# URL with basic auth
+url = f"https://download.maxmind.com/geoip/databases/GeoLite2-City-CSV/download?suffix=zip"
+request = urllib.request.Request(url)
+request.add_header('Authorization', f'Basic {base64_credentials}')
+
+# Download the file
+with urllib.request.urlopen(request) as response, open(ZIP_FILENAME, 'wb') as out_file:
+    shutil.copyfileobj(response, out_file)
+
+# Extract the downloaded zip file
 with zipfile.ZipFile(ZIP_FILENAME, 'r') as zip_ref:
     zip_ref.extractall(TEMPORAL_EXTRACTED_DIR)
 
